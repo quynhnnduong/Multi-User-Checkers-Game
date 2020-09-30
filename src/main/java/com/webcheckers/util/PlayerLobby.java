e86bf1776a1251f5dcf6e9c4e63b507c47b527b0
qnd5128 <qnd5128@g.rit.edu>
2020-09-30 09:49:57 -0400

    working on PlayerLobby

---
+++ src/main/java/com/webcheckers/util/PlayerLobby.java
@@ -0,0 +1,44 @@
+package com.webcheckers.util;
+
+import com.webcheckers.model.Player;
+
+import java.util.ArrayList;
+
+public class PlayerLobby {
+
+    /**
+     * Create an ArrayList for players log in
+     */
+    private ArrayList<Player> playerList = new ArrayList<Player>();
+
+    /**
+     * Signs a player in.
+     *
+     * @param name name of player
+     * @return player object
+     */
+    public Player player(String name) {
+        Player p = new Player(name);
+        playerList.add(p);
+        return p;
+    }
+
+
+    /**
+     * Adds player to the playerList.
+     * @param player player to add
+     */
+    public void add(Player player) {
+        playerList.add(player);
+    }
+
+    /**
+     * Gets the number of players log in.
+     * @return size of playerList
+     */
+    public int getPlayerCount(){
+        return playerList.size();
+    }
+
+}
+
