aa1a93796be53dcfc8178180b04bb70d90e0b2e4
Dmitry <des3358@rit.edu>
2020-10-08 14:18:29 -0400

    Renamed certain instance variables for readability (player2 ->opponent, player->currentPlayer) Removed the player2 instance variable. Created a ternary operator that sends different BoardView objects to different players that have their Pieces at the bottom Modified and improved the documentation

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,6 +1,5 @@
 package com.webcheckers.ui;
 
-
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.BoardView;
 import com.webcheckers.model.Player;
@@ -15,136 +14,141 @@ import java.util.logging.Logger;
 import static com.webcheckers.ui.PostSignInRoute.PLAYER_NAME_ATTR;
 
 /**
- * The UI Controller to GET the Game page.
- * @author Shubhang Mehrotra(sm9943)
- * Added getting the correct player object for both players
- * @author Joel Clyne
+ * This UI component represents the action of Players joining a checkers game. The
+ * handle() function executes when a Player clicks the 'Start a Game' button next to an
+ * opponent's name. The checkers game screen (game.ftl) should be visible after the execution
+ * (if the opponent has not joined a game already).
+ *
+ * @author Shubhang Mehrotra, Joel Clyne, Dmitry Selin
  */
 public class GetGameRoute implements Route {
-    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+
+    /** A logger used to send messages to the console - for testing purposes */
+    private static final Logger LOG = Logger.getLogger(GetGameRoute.class.getName());
+
+    /** The Template Engine that generates the '.ftl' pages */
     private final TemplateEngine templateEngine;
+
+    /** A server-wide Player Lobby that keeps track of all the players waiting to start a game */
     private final PlayerLobby playerLobby;
 
+    /** A model-tier component that houses and represents the checkers game */
+    private BoardView boardView;
 
-    private Message message = Message.info("Here"); //ignore for now (shubhang)
-
-    private Player player2;
-    private final BoardView boardView = new BoardView();
-
+    //TODO Add  functionality and documentation to map
     private final HashMap<String, Object> map = null;
 
-
+    /** An enumeration of the mode selected by the user to enter into */
     enum viewMode {
         PLAY,
         SPECTATOR,
         REPLAY
     }
 
+    /** The color (representing a Player) whose turn it is currently */
     enum activeColor{
         RED,
         WHITE
     }
 
     /**
-     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+     * Creates the Spark Route that handles all the "GET /game" requests.
      *
-     * @param templateEngine
-     *   the HTML template rendering engine
+     * @param templateEngine the HTML template rendering engine
      */
     public GetGameRoute(final TemplateEngine templateEngine, PlayerLobby playerLobby) {
         this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
         this.playerLobby = playerLobby;
-        //
+        boardView = new BoardView();
         LOG.config("GetGameRoute is initialized.");
     }
 
     /**
-     * Render the Game page
+     * This method is called to handle the "GET /game" request. Whenever a Player
+     * calls an opponent in for a game, this method handles the rendering of the game page
+     * (game.ftl).
      *
-     * @param request
-     *   the HTTP request
-     * @param response
-     *   the HTTP response
+     * @param request the HTTP request
+     * @param response the HTTP response
      *
-     * @return
-     *   the rendered HTML for the Game page
+     * @return the rendered HTML for the Game page
      */
     @Override
     public Object handle(Request request, Response response) throws Exception {
-
-        //gets the current player
         final Session session = request.session();
 
-        //makes the current player the red player
-        Player player = playerLobby.getPlayer(session.attribute(PLAYER_NAME_ATTR));
-        Player redPlayer;
+        // Gets the current player
+        Player currentPlayer = playerLobby.getPlayer(session.attribute(PLAYER_NAME_ATTR));
 
-        //gets the player2 from the url param opponent and makes them the white player
-        Player player2 = playerLobby.getPlayer(request.queryParams("opponent"));
-        Player whitePlayer;
+        // Gets the opponent from the URL and makes them the WHITE player
+        Player opponent = playerLobby.getPlayer(request.queryParams("opponent"));
 
+        // Logs a FINER invocation message
         LOG.finer("GetGameRoute is invoked.");
-        //
-        Map<String, Object> vm = new HashMap<>();
 
+        // Checks if opponent is not in an existing game
+        if (opponent.getIsMidGame() && !opponent.getCallingPlayer()) {
 
-        //System.out.println(player2.getName());
-        //check if the opponent is not currently playing a game
-        if (player2.getIsMidGame() && !player2.getCallingPlayer()){
-            //redirect and tell the current user that the person they picked is facing someone else
+            // Redirect and inform currentPlayer that their opponent is playing in a different game
             session.attribute(GetHomeRoute.LEGIT_OPPONENT, false);
             //response.redirect(WebServer.HOME_URL);
-        } else {
+        }
+        else {
 
-
-            //this makes the current player's state be in game according to the httpSession
-            //for showing the error message of choosing to play against someone who's already in game
+            // Sets the currentPlayer's state to be MID_GAME
             session.attribute(GetHomeRoute.MID_GAME_KEY, true);
 
-            //set the current and opponent's states to playing in playerLobby
-            playerLobby.startPlayer(player.getName());
-            //set player 2 to be called for a game
-            player2.startCallingPlayer();
-            //
+            // Set currentPlayer and opponent's states to PLAYING in playerLobby
+            playerLobby.startPlayer(currentPlayer.getName());
 
-            //then tell playerLobby those two players are opponents
-            playerLobby.setOpponentMatch(player, player2);
+            // Start calling opponent to the game
+            opponent.startCallingPlayer();
+
+            // Inform PlayerLobby that currentPlayer and opponent are now playing
+            playerLobby.setOpponentMatch(currentPlayer, opponent);
 
             //TODO whenever we get to coding the w in state, set all these to not playing, and remove the opponents from each other
         }
 
-        //check if current player came in from a refresh of home.ftl - aka someone clicked on them and now they're being forced into a game
-        //this is the only situation where they would already be mid game
-        if (player.getIsMidGame()){
-            //make the opponent the red player
-            redPlayer = player2;
-            //and the current player white
-            whitePlayer = player;
-        } else {
-            //put them in the midgame state
-            playerLobby.startPlayer(player2.getName());
-            //no longer called
-            player2.stopCallingPlayer();
-            //vice versa
-            redPlayer = player;
-            whitePlayer = player2;
+        Player redPlayer;
+        Player whitePlayer;
+
+        // Check if currentPlayer was called into a game by opponent
+        if (currentPlayer.getIsMidGame()){
+
+            // Opponent becomes the red Player since opponent started the game
+            redPlayer = opponent;
+            whitePlayer = currentPlayer;
+        }
+        else {
+
+            // Both Players get put into the MID_GAME state
+            playerLobby.startPlayer(opponent.getName());
+            opponent.stopCallingPlayer();
+
+            // CurrentPlayer called opponent, and therefore becomes the red Player
+            redPlayer = currentPlayer;
+            whitePlayer = opponent;
         }
 
+        // Creates the HashMap to house all the freemarker components
+        Map<String, Object> vm = new HashMap<>();
 
-            vm.put("title", "Game");
-            vm.put("currentUser", player);
-            vm.put("loggedIn", true);
-            vm.put("viewMode", viewMode.PLAY);
-            vm.put("modeOptionsAsJSON", map);
-            vm.put("redPlayer", redPlayer);
-            vm.put("whitePlayer", whitePlayer);
-            vm.put("activeColor", activeColor.RED);
-            vm.put("board", boardView);
-            vm.put("message", message);
+        // Adds all freemarker components to the HashMap
+        vm.put("title", "Game");
+        vm.put("currentUser", currentPlayer);
+        vm.put("loggedIn", true);
+        vm.put("viewMode", viewMode.PLAY);
+        vm.put("modeOptionsAsJSON", map);
+        vm.put("redPlayer", redPlayer);
+        vm.put("whitePlayer", whitePlayer);
+        vm.put("activeColor", activeColor.RED);
 
+        // The BoardView depends on the currentPlayer
+        // If the Player has white Pieces, flip the board to have the white Pieces at the bottom of the board
+        vm.put("board", (currentPlayer == redPlayer ? boardView : boardView.getFlippedBoard()));
 
-        // render the View
-        System.out.println(player2.getName());
+        // Render the Game Page
         return templateEngine.render(new ModelAndView(vm , "game.ftl"));
     }
 }
