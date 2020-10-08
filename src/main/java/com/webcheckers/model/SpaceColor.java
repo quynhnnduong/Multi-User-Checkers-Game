e876af0a865ab0fca2c2f3fabd3e79e05c24a70f
Dmitry <des3358@rit.edu>
2020-10-08 13:22:14 -0400

    Created an external enum representation for the color of checker board Space tiles - this was done for increased readability and organization.

---
+++ src/main/java/com/webcheckers/model/SpaceColor.java
@@ -0,0 +1,16 @@
+package com.webcheckers.model;
+
+/**
+ * This enum is a simple representation of the colors of the Space tiles
+ * found on a checkers board.
+ *
+ * @author Dmitry Selin
+ */
+public enum SpaceColor {
+
+    /** A representation of the white checkers board Spaces */
+    WHITE,
+
+    /** A representation of the black checkers board Spaces */
+    BLACK
+}
