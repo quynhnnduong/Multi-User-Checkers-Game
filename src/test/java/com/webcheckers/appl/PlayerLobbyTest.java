1550889752f8c734042b31b6318e2148f0c03d33
sm9943 <sm9943@rit.edu>
2020-10-13 01:59:45 +0530

    Created PlayerLobbyTest.java

---
+++ src/test/java/com/webcheckers/appl/PlayerLobbyTest.java
@@ -0,0 +1,14 @@
+package com.webcheckers.appl;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit Test suite for {@link PlayerLobby} component
+ * @author Shubhang Mehrotra
+ */
+@Tag("Application-tier")
+public class PlayerLobbyTest {
+}
