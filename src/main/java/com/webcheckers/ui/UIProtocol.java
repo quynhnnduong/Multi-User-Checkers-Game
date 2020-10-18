79a22a21b14d5f658863a7366d9f8cfd1aca62c5
Dmitry <des3358@rit.edu>
2020-10-18 16:43:20 -0400

    Updated certain attribute names

--- src/main/java/com/webcheckers/ui/UIProtocol.java
+++ src/main/java/com/webcheckers/ui/UIProtocol.java
@@ -5,7 +5,7 @@ public class UIProtocol {
     //tells whether to show the no log in, or log in screen
     public static final String LOGGED_IN_ATTR = "loggedIn";
 
-    //the object for displaying the player name (not sure why its not being used)
+    //the object for displaying the player (not sure why its not being used)
     public static final String PLAYER_ATTR = "player";
 
     //the object to display the current online players on the home page.
@@ -21,14 +21,14 @@ public class UIProtocol {
     public static final String BOARD_ATTR = "board";
 
     //Key in the session attribute map for the player who started the session stating that this player has interracted with the player lobby
-    public static final String PLAYERLOBBY_KEY = "playerLobby";
+    public static final String PLAYERLOBBY_ATTR = "playerLobby";
 
     //the key to determine if a player has entered a name that someone else has
-    public static final String LEGIT_NAME = "legitName";
+    public static final String LEGIT_NAME_ATTR = "legitName";
 
     //the key to determine if a player picked someone previously who wasn't already playing a game (or they didn't pick anyone)
-    public static final String LEGIT_OPPONENT = "legitOpponent";
+    public static final String LEGIT_OPPONENT_ATTR = "legitOpponent";
 
     //the key to determine if a player is currently playing a game
-    public static final String MID_GAME_KEY = "midGame";
+    public static final String MID_GAME_ATTR = "midGame";
 }
