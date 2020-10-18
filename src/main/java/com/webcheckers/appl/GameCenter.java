daf2a243443766eeebc40cd7e4da90b6d6935564
Dmitry <des3358@rit.edu>
2020-10-18 16:52:40 -0400

    Removed Functionality To maintain program functionality and high cohesion, the functionality from GameCenter was removed

--- src/main/java/com/webcheckers/appl/GameCenter.java
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -11,15 +11,4 @@ public class GameCenter {
     private PlayerLobby playerLobby;
 
     public GameCenter() { playerLobby = new PlayerLobby(this); }
-
-    /**
-     * Creates a hashcode to be used as a game ID based on the player's names
-     * @param currentPlayer
-     * @param opponent
-     * @return
-     */
-    public int generateGameID(Player currentPlayer, Player opponent){
-        String namesTogether = currentPlayer.getName() + opponent.getName();
-        return namesTogether.hashCode();
-    }
 }
