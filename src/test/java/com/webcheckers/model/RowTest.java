011cbafa2568dccb8403b63736049b9251775551
Sasha Persaud <srp4581@rit.edu>
2020-10-19 14:15:13 -0400

    stubbed out tests for Row

---
+++ src/test/java/com/webcheckers/model/RowTest.java
@@ -0,0 +1,41 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Unit Tests for {@link Row} Component
+ * @author Sasha Persaud
+ */
+@Tag("Model-tier")
+public class RowTest {
+    // Component under test
+    private Row CuT;
+
+    // Friendlies
+    private int index;
+    private ArrayList<Space> row;
+
+    // Attributes for mock objects
+    private SpaceColor spaceColor;
+
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @Test
+    public void testGenerateRow(){
+
+    }
+
+    @Test
+    public void testCreateRow(){
+
+    }
+}
