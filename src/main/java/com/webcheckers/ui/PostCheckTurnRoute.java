971d00428f35f2b5d95bf72f23781a721ba8b1fc
Dmitry <des3358@rit.edu>
2020-10-20 12:00:55 -0400

    Simplified PostCheckTurnRoute The class does not work as intended yet - it has just been stripped of most unnecessary components

--- src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
@@ -5,44 +5,24 @@ import com.webcheckers.model.Player;
 import com.webcheckers.util.Message;
 import spark.*;
 
-import java.util.logging.Logger;
-
 import static com.webcheckers.ui.UIProtocol.PLAYER_ATTR;
 
 public class PostCheckTurnRoute implements Route {
-
-    private static final Logger LOG = Logger.getLogger(PostCheckTurnRoute.class.getName());
-    private TemplateEngine templateEngine;
-
-    public PostCheckTurnRoute(TemplateEngine templateEngine){
-        this.templateEngine = templateEngine;
-        LOG.config("PostCheckTurnRoute is initialized");
-    }
-
 
     @Override
     public Object handle(Request request, Response response) {
 
         final Session session = request.session();
 
-        //TODO actually check if its the waiting player's turn, and add the opponent's name to queryparams
-        //for now assume its not the waiting player's turn
-
         Player currentPlayer = session.attribute(PLAYER_ATTR);
 
         Message message = Message.info("false");
 
-
-        // if it not the opponent's turn switch whoever's going
         if ( !currentPlayer.getOpponent().isTurn()){
             message = Message.info("true");
         }
 
-
-        Gson gson = new Gson();
-        String messageJson = gson.toJson(message);
-
-        return messageJson;
+        return new Gson().toJson(message);
 
     }
 }
