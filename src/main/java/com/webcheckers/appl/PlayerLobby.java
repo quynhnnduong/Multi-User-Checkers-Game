753b43f885aa25f130021014778025506e0fb551
Dmitry <des3358@rit.edu>
2020-10-20 12:26:15 -0400

    Started Fixing Documentation

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 /**
  * Holds all existing players and add new players
+ *
  * @author Quynh Duong, Sasha Persaud
  */
 public class PlayerLobby {
@@ -17,29 +18,23 @@ public class PlayerLobby {
      */
     private final HashMap<String, Player> players = new HashMap<>();
 
-    /**
-     * Holds a like to the one and only GameCenter
-     * @author Joel Clyne
-     */
     private final GameCenter gameCenter;
 
     public static final String PLAYER_LOBBY_MSG = " player(s) in lobby";
 
     public static final String NO_PLAYERS_MSG = "There are no other players available to play at this time.";
 
-    /**
-     * New PlayerLobby set up
-     */
     public PlayerLobby(GameCenter gameCenter){
         this.gameCenter = gameCenter;
     }
 
     /**
      * Adds new player to the players map.
+     *
      * @return boolean
      */
     public boolean addPlayer(Player player) {
-        if (player.isNameValid() && !players.containsKey(player.getName())) {
+        if (!players.containsKey(player.getName())) {
             players.put(player.getName(), player);
             return true;
         }
@@ -47,40 +42,29 @@ public class PlayerLobby {
         return false;
     }
 
-    /**
-     * Look for a Player in the PlayerLobby. If they are found, return the player.
-     * If they are not found, return null.
-     * @return player, null
-     */
     public Player getPlayer (String name) { return players.get(name); }
 
     /**
      * Gets the number of players logged in.
+     *
      * @return size of players
      */
-    public int getNumOfPlayers(){ return players.size(); }
+    public int getNumOfPlayers() { return players.size(); }
 
     /**
      * Removes player from the players map.
+     *
      * @param player player to remove
      */
-    public void removePlayer(Player player){ players.remove(player.getName()); }
+    public void removePlayer(Player player) { players.remove(player.getName()); }
 
     public ArrayList<Player> getPlayers() { return new ArrayList<>(players.values()); }
 
-    public void setOpponentMatch(Player  player1, Player player2){
+    public void setOpponentMatch(Player  player1, Player player2) {
         player1.setOpponent(player2);
         player2.setOpponent(player1);
     }
 
-    /**
-     * Get all of the players
-     * @return set of players
-
-    public Collection<Player> getAllPlayers() {
-        return players.values();
-    }
-     */
     public String getLobbyMessage() {
         int numOfPlayers = getNumOfPlayers();
 
@@ -93,11 +77,12 @@ public class PlayerLobby {
     /**
      * Prints all the players in the player lobby.
      * Purely for debugging purposes, we can remove it later!
+     *
      * @return String
-     * @author Sasha Persaud
      */
-    public String printPlayers(){
+    public String printPlayers() {
         StringBuilder playerString = new StringBuilder();
+
         for (String name : players.keySet()) {
             playerString.append(name);
             playerString.append(" ");
