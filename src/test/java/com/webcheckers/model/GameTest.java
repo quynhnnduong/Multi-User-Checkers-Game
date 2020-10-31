934f965c46f4b7429e8bade8db4d058233705a88
sm9943 <sm9943@rit.edu>
2020-10-31 19:45:56 +0530

    GameTest passes all current tests. More tests needed

--- src/test/java/com/webcheckers/model/GameTest.java
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -1,70 +1,68 @@
-//package com.webcheckers.model;
-//
-//import com.webcheckers.model.Game;
-//import com.webcheckers.model.Player;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Tag;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// * Unit Tests for {@link Game} Component
-// * @author Sasha Persaud
-// */
-////@Tag("Application-tier")
-//public class GameTest {
-//
-//    // Component under test
-//    private Game CuT;
-//
-//    // Friendlies
-//    private Player redPlayer;
-//    private Player whitePlayer;
-//    private int id;
-//
-////    @BeforeEach
-//    private void setUp(){
-//        redPlayer = new Player("RedPlayer");
-//        whitePlayer = new Player("WhitePlayer");
-//        id = 1234;
-//    }
-//
-////    @Test
-//    public void testCreateGame(){
-//        // Invoke
-//        CuT = new Game(redPlayer, whitePlayer, id);
-//
-//        // Analyze
-//        assertNotNull(CuT);
-//    }
-//
-////    @Test
-//    public void testGetRedPlayer(){
-//        // Invoke
-//        CuT = new Game(redPlayer, whitePlayer, id);
-//        Player testPlayer = CuT.getRedPlayer();
-//
-//        // Analyze
-//        assertEquals(redPlayer, testPlayer);
-//    }
-//
-////    @Test
-//    public void testGetWhitePlayer(){
-//        // Invoke
-//        CuT = new Game(redPlayer, whitePlayer, id);
-//        Player testPlayer = CuT.getWhitePlayer();
-//
-//        // Analyze
-//        assertEquals(whitePlayer, testPlayer);
-//    }
-//
-////    @Test
-//    public void testGetId(){
-//        // Invoke
-//        CuT = new Game(redPlayer, whitePlayer, id);
-//        int testID = CuT.getId();
-//
-//        // Analyze
-//        assertEquals(id, testID);
-//    }
-//}
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit Tests for {@link Game} Component
+ * @author Sasha Persaud, Shubhang Mehrotra
+ */
+@Tag("Model-tier")
+public class GameTest {
+
+    // Component under test
+    private Game CuT;
+
+    // Friendlies
+    private Player redPlayer;
+    private Player whitePlayer;
+    private String id;
+
+    @BeforeEach
+    private void setUp(){
+        redPlayer = new Player("RedPlayer");
+        whitePlayer = new Player("WhitePlayer");
+        id = "1234";
+    }
+
+    @Test
+    public void testCreateGame(){
+        // Invoke
+        CuT = new Game(id,redPlayer, whitePlayer);
+
+        // Analyze
+        assertNotNull(CuT);
+    }
+
+    @Test
+    public void testGetRedPlayer(){
+        // Invoke
+        CuT = new Game(id,redPlayer, whitePlayer);
+        Player testPlayer = CuT.getRedPlayer();
+
+        // Analyze
+        assertEquals(redPlayer, testPlayer);
+    }
+
+   @Test
+    public void testGetWhitePlayer(){
+        // Invoke
+        CuT = new Game(id, redPlayer, whitePlayer);
+        Player testPlayer = CuT.getWhitePlayer();
+
+        // Analyze
+        assertEquals(whitePlayer, testPlayer);
+    }
+
+    @Test
+    public void testGetId(){
+        // Invoke
+        CuT = new Game(id, redPlayer, whitePlayer);
+        String testID = CuT.getId();
+
+        // Analyze
+        assertEquals(id, testID);
+    }
+}
