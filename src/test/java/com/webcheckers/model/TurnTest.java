83f2e56ae0ae4d3a053e6b1840288da3526d4daf
sm9943 <sm9943@rit.edu>
2020-11-01 06:42:11 +0530

    Added TurnTest.java

---
+++ src/test/java/com/webcheckers/model/TurnTest.java
@@ -0,0 +1,13 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Tag;
+
+/**
+ * Unit Tests for {@link Turn} Component
+ *
+ * @author Shubhang Mehrotra
+ */
+@Tag("Model-tier")
+public class TurnTest {
+
+}
