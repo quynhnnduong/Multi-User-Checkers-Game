711e95351866449347b5856ea823d3057862ffc8
sm9943 <sm9943@rit.edu>
2020-11-01 09:48:02 +0530

    Added tests to TurnTest.java

--- src/test/java/com/webcheckers/model/TurnTest.java
+++ src/test/java/com/webcheckers/model/TurnTest.java
@@ -1,6 +1,13 @@
 package com.webcheckers.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit Tests for {@link Turn} Component
@@ -9,5 +16,25 @@ import org.junit.jupiter.api.Tag;
  */
 @Tag("Model-tier")
 public class TurnTest {
+
+    //CuT
+    private Turn CuT = new Turn();
+
+    //
+    private Move move;
+
+    @BeforeEach
+    public void setup() {
+        move = mock(Move.class);
+        ArrayList<Move> validMoves = new ArrayList<>();
+    }
+    @Test
+    public void testAddValidMove(){
+
+        CuT.addValidMove(move);
+
+        assertTrue(CuT.hasMoves());
+    }
+
 
 }
