64dd7257ebf2d04d4860cf5e00dbc7ddd33ad694
Dmitry <des3358@rit.edu>
2020-11-02 16:26:18 -0500

    Removed Unnecessary Lines

--- src/main/java/com/webcheckers/ui/UIProtocol.java
+++ src/main/java/com/webcheckers/ui/UIProtocol.java
@@ -8,17 +8,8 @@ package com.webcheckers.ui;
  */
 public class UIProtocol {
 
-    //tells whether to show the no log in, or log in screen
-    //public static final String LOGGED_IN_ATTR = "loggedIn";
-
     //the object for displaying the player (not sure why its not being used)
     public static final String PLAYER_ATTR = "player";
-
-    //the object to display the current online players on the home page.
-    //public static final String PLAYER_MSG_ATTR = "playersMessage";
-
-    //Key in the session attribute map for the player who started the session stating that this player has interacted with the player lobby
-    //public static final String PLAYERLOBBY_ATTR = "playerLobby";
 
     //the key to determine if a player has entered a name that someone else has
     public static final String LEGIT_NAME_ATTR = "legitName";
